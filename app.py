89d6a52bdaaa02d57bae30cdc3d4fac5f3dddd54
Inbal <doar001@gmail.com>
2023-06-02 18:57:26 +0300

    Nav bar with books only

--- app.py
+++ app.py
@@ -17,9 +17,15 @@ app.register_blueprint(books_blueprint, url_prefix='/books')
 app.register_blueprint(customers_blueprint, url_prefix='/customers')
 app.register_blueprint(loans_blueprint, url_prefix='/loans')
 
-@app.route('/<path:path>')
-def send_report(path):
-    return send_from_directory('static', path)
+# @app.route('/<path:path>')
+# def send_report(path):
+#     return send_from_directory('static', path)
+
+@app.route("/booksPage")
+def get_books():
+    # Your code to retrieve books from the database
+    # ...
+    return render_template("index1.html")
 
 if __name__ == "__main__":
     with app.app_context():
